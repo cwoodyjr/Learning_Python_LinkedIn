05586dbe61bc4c8bdfa33bf17e2cee5c81a1e061
Craig <craig.woodcock89@hotmail.co.uk>
2022-11-20 09:56:10 +0000

    helloworld_start helloworld.py complete

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -1,6 +1,14 @@
 #
 # Example file for HelloWorld
 # LinkedIn Learning Python course by Joe Marini
+
+def main():
+    print("hello World")
+    name = input("what is your name?")
+    print("hello",name)
+
+if  __name__ == "__main__":
+    main()
 #
 
 
