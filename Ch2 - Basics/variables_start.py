5b017e3ebc87ca93e5da2d39befcb5855027d47d
Craig <craig.woodcock89@hotmail.co.uk>
2022-11-20 10:28:52 +0000

    variables_start Variable complete

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -26,17 +26,28 @@ myint = "abc"
 print(myint)
 # to access a member of a sequence type, use []
 print(mylist[2])
-print(mylist[1:4])
-print(mylist[0:5:2])
-print(mylist[::-1]) #prints lst in reverse
 
 # use slices to get parts of a sequence
+print(mylist[1:4])
+print(mylist[0:5:2])
 
 # you can use slices to reverse a sequence
+print(mylist[::-1]) #prints lst in reverse
 
 # dictionaries are accessed via keys
-
+print(mydict)
+print(mydict["two"])
 # ERROR: variables of different types cannot be combined
-
+print("this is a string"+ str(123))
 # Global vs. local variables in functions
 
+def thisIsAFunction():
+    global mystr  #this line makes mystr a global variable so it can be accessed from anywhere in the file
+    mystr = "def"
+    print(mystr)
+
+thisIsAFunction()
+print(mystr)
+
+del(mystr) #this line deletes the variable
+print(mystr) #this line throws an error as the variable no longer exists
