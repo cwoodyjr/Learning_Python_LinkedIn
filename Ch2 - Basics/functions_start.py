4c44293f36f5879868e1526fb694eeff01e62dbb
Craig <craig.woodcock89@hotmail.co.uk>
2022-11-20 09:46:56 +0000

    hello world.py

---
+++ Ch2 - Basics/functions_start.py
@@ -0,0 +1,27 @@
+#
+# Example file for working with functions
+# LinkedIn Learning Python course by Joe Marini
+#
+
+
+# TODO: define a basic function
+def main():
+    print("hello World")
+    name = input("what is your name?")
+    print("hello",name)
+
+if  __name__ == "__main__":
+    main()
+
+# TODO: function that takes arguments
+
+
+# TODO: function that returns a value
+
+
+# TODO: function with default value for an argument
+
+
+# TODO: function with variable number of arguments
+
+
